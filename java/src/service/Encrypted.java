b9b3f8914dec5b2e27e1b42dd4cc0d653a10d15a
Eondoa <eric.ondoa.ext@direct-energie.com>
2016-06-15 14:39:42 +0200

    add service to encrypted word

---
+++ java/src/service/Encrypted.java
@@ -0,0 +1,17 @@
+package service;
+
+/**
+ * Created by Eondoa on 15/06/2016.
+ */
+public class Encrypted {
+
+	public StringBuilder encriyptedWord(String input, int key){
+		StringBuilder result = new StringBuilder();
+
+		String alphabet = "ABCDEFGHIJKLMNOPQRSTVWXYZ";
+
+
+		return result;
+	}
+
+}
